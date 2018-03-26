824dfb2ae58e1bf932b8bfebca16588ad9a946f0
Ryan Tan <ryan.tan@ericsson.com>
2018-03-26 17:09:13 +0800

    Add python template file

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''Docstring for this module'''
+
+
+def main():
+    '''entry point'''
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
